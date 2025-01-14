a38eff949c6e65ba3cd15dc616e6abbb78b80133
BomberQLF <tmurphyparis@gmail.com>
2025-01-14 21:08:13 +0100

    Navbar in creation

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const Navbar = () => {
+
+    // const [isDarkMode, setDarkMode] = useState(false);
+    const [isDarkMode, setDarkMode] = useState(false);
+
+    const handleToggleMode = () => {
+        setDarkMode(!isDarkMode);
+    };
+
+    return (
+        <nav className="p-4">
+            <ul className="flex justify-between">
+                <li className="text-sm">Where in the world?</li>
+                <li>
+                    <div className="dark-mode-container flex">
+                        <img className="pr-2" src="/src/assets/moon.png" alt="Light Dark mode" />
+                        <span className="text-sm" onClick={handleToggleMode}>{isDarkMode ? 'Dark Mode' : 'Light Mode'}</span>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
