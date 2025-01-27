34a19618deff9efb6a658b37c26e86a46bb2912f
BomberQLF <tmurphyparis@gmail.com>
2025-01-27 23:00:22 +0100

    Mini avancée

--- src/Components/CountryDetail.tsx
+++ src/Components/CountryDetail.tsx
@@ -23,19 +23,21 @@ const CountryDetail = () => {
                     </Link>
                 </div>
 
-                <div className="big-detail-container">
+                <div className="big-detail-container lg:flex lg:justify-between">
                     <div className="mt-10">
                         <img src={location.state.flags.svg} alt={"Flag of " + location.state.name} />
                     </div>
-                    <div className="mt-8">
-                        <h1 className="text-2xl text-white font-medium">{location.state.name}</h1>
-                    </div>
-                    <div className="mt-6">
-                        <p className="text-white">Native Name :<span className="font-thin"> {location.state.nativeName}</span></p>
-                        <p className="text-white">Population :<span className="font-thin"> {location.state.population}</span></p>
-                        <p className="text-white">Region :<span className="font-thin"> {location.state.region}</span></p>
-                        <p className="text-white">Sub Region :<span className="font-thin"> {location.state.subregion}</span></p>
-                        <p className="text-white">Capital :<span className="font-thin"> {location.state.capital}</span></p>
+                    <div className="wrapper">
+                        <div className="mt-8">
+                            <h1 className="text-2xl text-white font-medium">{location.state.name}</h1>
+                        </div>
+                        <div className="mt-6">
+                            <p className="text-white">Native Name :<span className="font-thin"> {location.state.nativeName}</span></p>
+                            <p className="text-white">Population :<span className="font-thin"> {location.state.population}</span></p>
+                            <p className="text-white">Region :<span className="font-thin"> {location.state.region}</span></p>
+                            <p className="text-white">Sub Region :<span className="font-thin"> {location.state.subregion}</span></p>
+                            <p className="text-white">Capital :<span className="font-thin"> {location.state.capital}</span></p>
+                        </div>
                     </div>
                 </div>
             </div>
